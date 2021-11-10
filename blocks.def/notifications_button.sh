0bc8492c51a3894ddf3bfd68f73483e547de9cb0
Santiago Lo Coco <santilococo.01@gmail.com>
2021-11-09 23:58:51 -0300

    Update notif block

--- blocks.def/notifications_button.sh
+++ blocks.def/notifications_button.sh
@@ -4,12 +4,16 @@ toggleNotifications() {
     dunstctl set-paused toggle 
 }
 
+getNotifications() {
+    dunstctl history-pop
+}
+
 refreshNotificationsBlock() {
     sigdwmblocks 6
 }
 
 case "$1" in
-    #1) toggleNotifications ;;
+    1) getNotifications ;;
     2) toggleNotifications ;;
     #3) pactl set-sink-mute @DEFAULT_SINK@ toggle;;
 esac
